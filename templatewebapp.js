25c0cb7a0b876a9ea4f1ced5365998c4bd11bdaa
golfnoonrin <153844686+golfnoonrin@users.noreply.github.com>
2024-02-28 00:18:34 +0700

    Update templatewebapp.js

--- templatewebapp.js
+++ templatewebapp.js
@@ -1,2 +1,16 @@
 
-console.log("ทดสอบ cdn2")
+function showLastView(){
+  const lastViewId = sessionStorage.getItem("sLastView") 
+  console.log(lastViewId)
+
+  if ( lastViewId != null){
+    showViewById(lastViewId)
+  } 
+  else {
+    const id = "home_view_btn"
+    showViewById(id)
+  }
+
+  // loadingEnd()
+
+};
